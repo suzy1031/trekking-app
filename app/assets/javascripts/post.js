692ac5e8f75bfc310d6a4218a3414a5bb01b45c1
suzy1031 <kohei.sugawara.1031@gmail.com>
2019-10-09 21:13:44 +0900

    change css & Js

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,16 +1,22 @@
 $(function() {
   var $textarea = $('#textarea');
   var $textarea2 = $('#textarea2');
+  var $textarea3 = $('#textarea3');
   var lineHeight = parseInt($textarea.css('lineHeight'));
   $textarea.on('input', function(e) {
     var lines = ($(this).val() + '\n').match(/\n/g).length;
     $(this).height(lineHeight * lines);
   });
-  // Meal編集画面２個目のtextarea用Js
+  // 2個目のtextarea用Js
   $textarea2.on('input', function(e) {
     var lines = ($(this).val() + '\n').match(/\n/g).length;
     $(this).height(lineHeight * lines);
   });
+    // 3個目のtextarea用Js
+    $textarea3.on('input', function(e) {
+      var lines = ($(this).val() + '\n').match(/\n/g).length;
+      $(this).height(lineHeight * lines);
+    });
   //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
   $('.photo-form').on('change', 'input[type="file"]', function(e) {
     var file = e.target.files[0],
@@ -40,6 +46,28 @@ $(function() {
 
     reader.readAsDataURL(file);
   });
+  // posts#new 山メシ画像用
+  $('.photo-form2').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".preview2");
+        t = this;
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  width: "300px",
+                  class: "preview2",
+                  title: file.name
+              }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
   // 編集する時に画像をpreviewさせる=>変更後の画像をpreviewさせる
   $(function() {
     function readURL(input) {
@@ -54,7 +82,7 @@ $(function() {
     $("#post_img").change(function(){
       readURL(this);
     });
-   });
+  });
 });
 // 削除する時に確認するwindowを表示
 $(function() {
