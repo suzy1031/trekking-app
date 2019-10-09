abeae0bd31381614b2ed85ebda96cbe99b4eb6e4
suzy1031 <kohei.sugawara.1031@gmail.com>
2019-10-09 12:31:11 +0900

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,31 @@
-20.times do |i|
-  User.create!(email: "test#{i + 1}@example.com", password: "password", password_confirmation: "password", nickname: "ニックネーム#{i + 1}", profile: "宜しくお願いします！")
+10.times do |i|
+  i += 1
+  user = User.create!(
+    email: "user#{i}@example.com",
+    password: 'password',
+    password_confirmation: "password",
+    nickname: "ニックネーム#{i}",
+    profile: "宜しくお願いします！"
+  )
+
+  3.times do |j|
+    users = User.order(:created_at)
+    j += 1
+    users.each{user.posts.create!(name:"テスト山#{j}",
+      text: "楽しい山登り！",
+      image: open("#{Rails.root}/app/assets/images/利尻山.png"),
+      elevation: "87#{j}",
+      walking_distance: "1#{j}",
+      difficulty: rand(3)
+    )}
+
+    users.each{user.likes.create!(post_id: i, user_id: j)}
+  end
 end
 
-users = User.order(:created_at)
-6.times do |i|
-  text = Faker::Lorem.sentence
-  users.each{ |user| user.posts.create!(name:"テスト山#{i + 1}", text: text, image: open("#{Rails.root}/app/assets/images/利尻山.png"), elevation: "87#{i + 1}", walking_distance: "1#{i + 1}", difficulty: rand(3)) }
-end
+users = User.all
+user  = users.first
+following = users[2..9]
+followers = users[3..8]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
