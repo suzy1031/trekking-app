42064090cf1f16c6394f474460dabefe17262195
suzy1031 <kohei.sugawara.1031@gmail.com>
2019-10-14 13:10:14 +0900

    add data seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,39 @@
 10.times do |i|
   i += 1
   user = User.create!(
-    email: "user#{i}@example.com",
-    password: 'password',
-    password_confirmation: "password",
-    nickname: "ニックネーム#{i}",
-    profile: "宜しくお願いします！"
+    email:                  "user#{i}@example.com",
+    password:               "password",
+    password_confirmation:  "password",
+    nickname:               "ニックネーム#{i}",
+    profile:                "宜しくお願いします！",
+    image:                  open("#{Rails.root}/db/fixtures/ユーザー.jpeg")
   )
-
   3.times do |j|
     users = User.order(:created_at)
     j += 1
-    users.each{user.posts.create!(name:"テスト山#{j}",
-      text: "楽しい山登り！",
-      image: open("#{Rails.root}/app/assets/images/利尻山.png"),
-      elevation: "87#{j}",
-      walking_distance: "1#{j}",
-      difficulty: rand(3)
+    users.each{user.posts.create!(
+      name:                 "テスト山#{j}",
+      text:                 "楽しい山登り！",
+      image:                open("#{Rails.root}/db/fixtures/利尻山.jpeg"),
+      elevation:            "87#{j}",
+      walking_distance:     "1#{j}",
+      difficulty:           rand(3)
     )}
 
     users.each{user.likes.create!(post_id: i, user_id: j)}
   end
+end
+
+10.times do |m|
+  m += 1
+  Meal.create!(
+    post_id:                "#{m}",
+    name:                   "テスト飯#{m}",
+    image:                  open("#{Rails.root}/db/fixtures/カレー.jpeg"),
+    cooking_time:           "1#{m}",
+    food_stuff:             "じゃがいも。人参。玉ねぎ",
+    cooking_method:          "お湯を沸かして野菜を煮込む。"
+  )
 end
 
 users = User.all
