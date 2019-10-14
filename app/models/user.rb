85f8191d4b38e995a4e7e41a9d2381c9b8cf10a1
suzy1031 <kohei.sugawara.1031@gmail.com>
2019-10-14 19:18:41 +0900

    setting user-validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts, dependent: :destroy
-  has_many :meals, dependent: :destroy
-  has_many :comments
-  has_many :likes, dependent: :destroy
-  validates :nickname, :profile, presence: true
+  has_many :posts,    dependent: :destroy
+  has_many :meals,    dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes,    dependent: :destroy
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email,     presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :nickname,  presence: true, length: {maximum: 20}
+  validates :profile,   presence: true
+  validates :password,  presence: true, length: {minimum: 6, maximum: 128}
 
   has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
