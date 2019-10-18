ddd5481d215aaf5ce851bbfab9668b2ba77c49fd
suzy1031 <kohei.sugawara.1031@gmail.com>
2019-10-18 20:52:02 +0900

    set validation in models

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,6 @@
 class Meal < ApplicationRecord
   belongs_to :post
   validates :name, :cooking_time, presence: true
+  validates :cooking_time, numericality: { only_integer: true }
   mount_uploader :image, ImageUploader
 end
