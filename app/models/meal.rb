452c94bff3e28b02fc10c8da030d635a64e907a1
suzy1031 <kohei.sugawara.1031@gmail.com>
2019-10-19 21:25:48 +0900

    text_field  refactoring

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,5 @@
 class Meal < ApplicationRecord
   belongs_to :post
   validates :name, :cooking_time, presence: true
-  validates :cooking_time, numericality: { only_integer: true }
   mount_uploader :image, ImageUploader
 end
