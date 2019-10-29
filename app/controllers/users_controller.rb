f92f38117e84b083d74cfee7d4a71fb39f639e10
suzy1031 <kohei.sugawara.1031@gmail.com>
2019-10-29 15:07:59 +0900

    create user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.order('updated_at DESC')
+  end
+
   def show
     @user = User.find( params[:id] )
     @posts = @user.posts.order("created_at DESC").page(params[:page]).per(5)
