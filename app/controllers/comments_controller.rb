dcec25ccd60040cd4d44aa7f28e3f837e80181ec
suzy1031 <kohei.sugawara.1031@gmail.com>
2019-10-30 15:37:08 +0900

    update comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,24 @@
 class CommentsController < ApplicationController
 
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(comment_params)
+    @comment = Comment.create(comment_params)
     if @comment.save
-      redirect_back(fallback_location: root_path)
+      flash[:success] = "コメントが完了しました！"
+      redirect_to post_path(@post.id)
     else
-      redirect_back(fallback_location: root_path)
+      flash.now[:alert] = "失敗しました。"
+        redirect_back(fallback_location: root_path)
     end
-
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id)
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
