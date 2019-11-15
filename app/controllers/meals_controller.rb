f3e7b0d2ef4342f5a17c13a7cf4c7b388778f02c
suzy1031 <kohei.sugawara.1031@gmail.com>
2019-11-15 21:27:13 +0900

    update view file

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -4,6 +4,7 @@ class MealsController < ApplicationController
     @meals = Meal.includes(:post).order('updated_at DESC')
   end
   def show
+    @title = "投稿詳細"
     @post = Post.find(params[:id])
     @meal = Meal.find(params[:id])
   end
