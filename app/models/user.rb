a2f50250082da024b50b0ee97cbfeaaa36c9009e
suzy1031 <kohei.sugawara.1031@gmail.com>
2019-11-17 18:26:45 +0900

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts,    dependent: :destroy
+  has_many :meals,    dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes,    dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
