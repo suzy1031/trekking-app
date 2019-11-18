5a6265daabceff3bbacf017a5b226511a26e39a5
suzy1031 <kohei.sugawara.1031@gmail.com>
2019-11-18 14:37:22 +0900

    update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,23 +8,39 @@ module ApplicationHelper
     end
   end
 
-  def yama_horizontal_list_image(post)
-    if post.image.present?
-      image_tag(post.image.url, class:"box-left__picture")
+  def user_posts_meals_has?
+    @user.posts.any? || @user.meals.any?
+  end
+
+  def posts_and_meals_total_count
+    @user.posts.count + @user.meals.count
+  end
+
+  def horizontal_list_image(instance)
+    if instance.image.present?
+      image_tag(instance.image.url, class:"box-left__picture")
     else
       image_tag("https://mgs01y1.wowma.net/pc/img/common/no_image300.gif?query=201908071342", class:"box-left__picture")
     end
   end
 
-  def show_user_image
-    if @user.image.present?
-      image_tag(@user.image.url, class:"user-face")
+  def sp_index_list_image(instance)
+    if instance.image.present?
+      image_tag(instance.image.url, class:"sp-post-box__picture")
     else
-      image_tag("https://static.mercdn.net/images/member_photo_noimage_thumb.png", class:"user-face")
+      image_tag("https://mgs01y1.wowma.net/pc/img/common/no_image300.gif?query=201908071342", class:"sp-post-box__picture")
     end
   end
 
-  def lists_user_image(user)
+  def meshi_show_user_image(post)
+    if post.user.image.present?
+      image_tag(post.user.image.url, class:"user-info__face")
+    else
+      image_tag("https://static.mercdn.net/images/member_photo_noimage_thumb.png", class:"user-info__face")
+    end
+  end
+
+  def circle_user_image(user)
     if user.image.present?
       image_tag(user.image.url, class:"user-face")
     else
@@ -32,9 +48,9 @@ module ApplicationHelper
     end
   end
 
-  def sp_show_user_image
-    if @user.image.present?
-      image_tag(@user.image.url, class:"sp-detail__user__picture")
+  def sp_circle_user_image(user)
+    if user.image.present?
+      image_tag(user.image.url, class:"sp-detail__user__picture")
     else
       image_tag("https://static.mercdn.net/images/member_photo_noimage_thumb.png", class:"sp-detail__user__picture")
     end
@@ -48,21 +64,6 @@ module ApplicationHelper
     end
   end
 
-  def sp_show_follow_user_image(user)
-    if user.image.present?
-      image_tag(user.image.url, class:"sp-post-box__picture")
-    else
-      image_tag("https://static.mercdn.net/images/member_photo_noimage_thumb.png", class:"sp-post-box__picture")
-    end
-  end
-
-  def sp_index_user_image(user)
-    if user.image.present?
-      image_tag(user.image.url, class:"sp-post-box__picture")
-    else
-      image_tag("https://static.mercdn.net/images/member_photo_noimage_thumb.png", class:"sp-post-box__picture")
-    end
-  end
 
   def yama_post_list_image(post)
     if post.image.present?
@@ -77,30 +78,6 @@ module ApplicationHelper
       image_tag(post.user.image.url, class:"ranking-user-image")
     else
       image_tag("https://static.mercdn.net/images/member_photo_noimage_thumb.png", class:"ranking-user-image")
-    end
-  end
-
-  def meshi_horizontal_list_image(meal)
-    if meal.image.present?
-      image_tag(meal.image.url, class:"box-left__picture")
-    else
-      image_tag("https://mgs01y1.wowma.net/pc/img/common/no_image300.gif?query=201908071342", class:"box-left__picture")
-    end
-  end
-
-  def meshi_index_page_image(meal)
-    if meal.image.present?
-      image_tag(meal.image.url, class:"sp-post-box__picture")
-    else
-      image_tag("https://mgs01y1.wowma.net/pc/img/common/no_image300.gif?query=201908071342", class:"sp-post-box__picture")
-    end
-  end
-
-  def yama_index_list_image(post)
-    if post.image.present?
-      image_tag(post.image.url, class:"sp-post-box__picture")
-    else
-      image_tag("https://mgs01y1.wowma.net/pc/img/common/no_image300.gif?query=201908071342", class:"sp-post-box__picture")
     end
   end
 
@@ -128,49 +105,17 @@ module ApplicationHelper
     end
   end
 
-  def meshi_show_user_image
-    if @post.user.image.present?
-      image_tag(@post.user.image.url, class:"user-info__face")
-    else
-      image_tag("https://static.mercdn.net/images/member_photo_noimage_thumb.png", class:"user-info__face")
-    end
-  end
-
-  def post_show_image
-    if @post.image.present?
-      image_tag(@post.image.url, class:"detail-container__picture")
+  def post_show_image(instance)
+    if instance.image.present?
+      image_tag(instance.image.url, class:"detail-container__picture")
     else
       image_tag("https://mgs01y1.wowma.net/pc/img/common/no_image300.gif?query=201908071342", class:"detail-container__picture")
     end
   end
 
-  def meshi_show_image
-    if @meal.image.present?
-      image_tag(@meal.image.url, class:"detail-container__picture")
-    else
-      image_tag("https://mgs01y1.wowma.net/pc/img/common/no_image300.gif?query=201908071342", class:"detail-container__picture")
-    end
-  end
-
-  def sp_meshi_show_user_image
-    if @post.user.image.present?
-      image_tag(@post.user.image.url, class:"sp-detail__user__picture")
-    else
-      image_tag("https://static.mercdn.net/images/member_photo_noimage_thumb.png", class:"sp-detail__user__picture")
-    end
-  end
-
-  def sp_meshi_show_image
-    if @meal.image.present?
-      image_tag(@meal.image.url, class:"sp-detail__pic-box__picture")
-    else
-      image_tag("https://mgs01y1.wowma.net/pc/img/common/no_image300.gif?query=201908071342", class:"sp-detail__pic-box__picture")
-    end
-  end
-
-  def sp_yama_show_image
-    if @post.image.present?
-      image_tag(@post.image.url, class:"sp-detail__pic-box__picture")
+  def sp_show_image(instance)
+    if instance.image.present?
+      image_tag(instance.image.url, class:"sp-detail__pic-box__picture")
     else
       image_tag("https://mgs01y1.wowma.net/pc/img/common/no_image300.gif?query=201908071342", class:"sp-detail__pic-box__picture")
     end
